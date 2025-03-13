3b3e19fd4ee68ed79daebfcbdeadf3ad52996b19
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-03-13 08:46:57 +0700

    Update Bina Service SO

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
         # SALES ORDER
         'report/lww_sales_report.xml',
         'views/lww_so_template.xml',
-        # 'views/bs_so_template.xml',
+        'views/bs_so_template.xml',
         # 'views/spartadua_so_template.xml',
         # FORM VIEW
         'views/sale_order_view.xml',
